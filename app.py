f0c30e3a7ed2b862b180330b9d18152f90a02192
Alejandro <lamedinaa@gmail.com>
2022-02-01 16:57:05 -0500

    inicio del front

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__,template_folder="Templates")
 def login():
     return render_template("Front/login.html")
 
+@app.route("/escritorio")
+def escritorio():
+    return render_template("App/escritorioUsuario.html")
+
 
 
 if __name__=='__main__':
