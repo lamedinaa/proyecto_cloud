b4d2b27276674e262fc084d3b2a5daba89f80088
Alejandro <lamedinaa@gmail.com>
2022-01-31 19:36:21 -0500

    Creacion app

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-##app
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+    return render_template("front/login.html")
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
